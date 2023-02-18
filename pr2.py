80a2449c6616b93c6b79533d94b4c4f3c317a03a
Pavel Dmitriev <35690354+paveldm@users.noreply.github.com>
2023-02-18 12:00:44 +0300

    Add files via upload

--- pr2.py
+++ pr2.py
@@ -1,13 +1,12 @@
 import math
 
-def aaa(y):
+def main(y):
     if(y < 62):
         return 99 * y**2 - 66 * y**5
-    elif(y < 94):
+    elif(y < 94 and y >= 62):
         return math.cos(y)**3 / 50 + 63 * \
                math.atan(70 * y + 27 * y**3) + y**6
     else:
         return math.tan(1 - 97 * y - 76 * y**2)**5 + \
                (y**3 - 1)**6 + math.tan(y)**2
 
-print aaa(-21)
