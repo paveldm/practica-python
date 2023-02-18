dc56877f88443cf3f3b1362c4a4b3de2e1ae2728
Pavel Dmitriev <35690354+paveldm@users.noreply.github.com>
2023-02-18 11:56:49 +0300

    Add files via upload

---
+++ pr2.py
@@ -0,0 +1,13 @@
+import math
+
+def aaa(y):
+    if(y < 62):
+        return 99 * y**2 - 66 * y**5
+    elif(y < 94):
+        return math.cos(y)**3 / 50 + 63 * \
+               math.atan(70 * y + 27 * y**3) + y**6
+    else:
+        return math.tan(1 - 97 * y - 76 * y**2)**5 + \
+               (y**3 - 1)**6 + math.tan(y)**2
+
+print aaa(-21)
