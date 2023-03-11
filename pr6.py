a96c78f618a64825c3adfb652752cec62bb2fc79
Pavel Dmitriev <35690354+paveldm@users.noreply.github.com>
2023-03-11 12:04:11 +0300

    Add files via upload

--- pr6.py
+++ pr6.py
@@ -1,28 +1,25 @@
 def main(r):
-    if r[4] == 'MUPAD':
-        return 12
-    if r[4] == 'UNO' and r[3] == 'RUST':
-        return 7
-    if r[0] == 'MESON':
-        return 0
-    if r[2] == 'NINJA':
-        return 11
-    for i in range(1, 11):
+    for i in range(13):
         flag = 0
-        for j in range(4):
+        for j in range(5):
+            if(s[i][j] == -1):
+                continue
             if r[j] != s[i][j]:
                 flag = 1
         if flag == 0:
             return i
 
 
-s = [['MUF', 1991, 'M', 'RED', 'UNO'],
-     ['MUF', 1976, 'M', 'RED', 'UNO'],
-     ['MUF', 1984, 'M', 'RED', 'UNO'],
-     ['NL', 1991, 'M', 'RED', 'UNO'],
-     ['NL', 1976, 'M', 'RED', 'UNO'],
-     ['NL', 1984, 'M', 'RED', 'UNO'],
-     ['MUF', 1991, 'M', 'NIX', 'UNO'],
-     ['MUF', 1976, 'M', 'NIX', 'UNO'],
-     ['MUF', 1984, 'M', 'NIX', 'UNO']]
-print(main(['MESON', 1991, 'M', 'NIX', 'UNO']))
+s = [['MESON', -1, -1, 'RED', 'UNO'],
+     ['MUF', 1991, -1, 'RED', 'UNO'],
+     ['MUF', 1976, -1, 'RED', 'UNO'],
+     ['MUF', 1984, -1, 'RED', 'UNO'],
+     ['NL', 1991, -1, 'RED', 'UNO'],
+     ['NL', 1976, -1, 'RED', 'UNO'],
+     ['NL', 1984, -1, 'RED', 'UNO'],
+     [-1, -1, -1, 'RUST', 'UNO'],
+     [-1, 1991, 'M', 'NIX', 'UNO'],
+     [-1, 1976, 'M', 'NIX', 'UNO'],
+     [-1, 1984, 'M', 'NIX', 'UNO'],
+     [-1, -1, 'NINJA', 'NIX', 'UNO'],
+     [-1, -1, -1, -1, 'MUPAD']]
